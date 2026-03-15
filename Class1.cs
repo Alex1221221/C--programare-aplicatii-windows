b5c5dca3ce94edae53ba694737dd1abbbc84ba92
Alex1221221 <alexandrucoman53@gmail.com>
2026-03-15 13:37:50 +0200

    Rezolvare problema 1

---
+++ Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class1
+    {
+        string Nume { get; set; }
+        string Grupa { get; set; }
+        double? NotaExamen { get; set; }
+
+        double? NotaLaborator { get; set; }
+
+        public Class1(string nume, string grupa, double? notaExamen,double? notaLaborator)
+        {
+            Nume = nume;
+            Grupa = grupa;
+            NotaExamen = notaExamen;
+            NotaLaborator = notaLaborator;
+        }
+
+        public double? Medie => (NotaExamen != null && NotaLaborator != null) ? (NotaExamen + NotaLaborator) / 2 : null;
+        
+        public void Afiseaza()
+        {
+            Console.WriteLine($"Studentul {Nume} are grupa {Grupa} , nota la examen: {NotaExamen ?? 0} si nota laborator: {NotaLaborator ?? 0}");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1[] s = new Class1[] {
+                new Class1("Andrei","1049",5,7),
+                new Class1("Alex", "1050", 6, 4),
+                new Class1("Maria","1049",9,2),
+                new Class1("Silvia","1060",7,null)
+
+            };
+            double sumaMedii = 0;
+            int promovati = 0;
+           foreach (Class1 c in s)
+            {
+                c.Afiseaza();
+                double medieSimpla = c.Medie ?? 0;
+                sumaMedii += medieSimpla;
+                if (medieSimpla >= 5) promovati++;
+            }
+            Console.WriteLine($"Nr de studenti care au promovat este: {promovati}");
+            Console.WriteLine($"Media generala este: {sumaMedii / s.Length}");
+        }
+    }
+} 
+
